4267bef9b3539c216fc83fd9f010b17954ef92a3
Domchap90 <domchaple@gmail.com>
2021-02-04 10:33:19 +0000

    Admin for blog class created and registered.

--- cms/admin.py
+++ cms/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Blog
 
 
-# Register your models here.
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+
+    list_display = ('blog_post_date', 'id', 'blog_title', 'blog_file')
+
+    ordering = ('-blog_post_date', 'blog_title')
+
+
+admin.site.register(Blog, BlogAdmin)
