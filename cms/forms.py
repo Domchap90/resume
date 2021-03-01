d9e9cdf6f69c973caae6d50bdb5ef642806a43fa
Domchap90 <domchaple@gmail.com>
2021-03-01 11:03:39 +0000

    Unique number validation enforced.

--- cms/forms.py
+++ cms/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from phonenumber_field.formfields import PhoneNumberField
 
+from .models import Subscriber
+
 
 class SubscriberForm(forms.Form):
     id = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -9,14 +11,23 @@ class SubscriberForm(forms.Form):
         max_length=80, label='Email Address')
     number = PhoneNumberField(label='Contact Number', required=False)
 
+    def clean_number(self):
+        number = self.cleaned_data['number']
+        for sub in Subscriber.objects.all():
+            if number == sub.number:
+                raise forms.ValidationError(
+                    "This number already exists within our records.")
+
+        return number
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         placeholders = {
             'id': '',
-            'name': "User's name",
-            'number': "If user wishes to be called (Optional)",
-            'email': "Allows user to be emailed",
+            'name': "John Smith",
+            'number': "+44 1632 960963 (Optional)",
+            'email': "example@domain.co.uk",
         }
 
         for field in self.fields:
