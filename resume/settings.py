61f6d9d33e9db2707274c9ba959d88b3a5066b1d
Domchap90 <domchaple@gmail.com>
2021-03-04 10:53:13 +0000

    Postgres DB set up for deployment.

--- resume/settings.py
+++ resume/settings.py
@@ -1,6 +1,6 @@
 import os
-# import dj_database_url
-from pathlib import Path
+import dj_database_url
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,7 +10,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = True
+# os.environ.get('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['localhost']
 
@@ -76,12 +77,20 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'TEST': {
+                'NAME': 'testdb',
+            },
+        }
+    }
 
 
 # Password validation
