fd84924342599391a5a8463a685298ca3cfdb2dc
Domchap90 <domchaple@gmail.com>
2021-03-27 12:18:13 +0000

    Labels adjusted for required fields.

--- home/forms.py
+++ home/forms.py
@@ -4,16 +4,16 @@ from django.core.validators import validate_email
 
 class ContactForm(forms.Form):
     name = forms.CharField(
-        label='Name', max_length=30,
+        label='Name*', max_length=30,
         widget=forms.TextInput(attrs={
             'placeholder': 'John Smith'}))
     phone = forms.CharField(
-        label='Contact Number', min_length=10, max_length=15,
+        label='Contact Number', min_length=10, max_length=15, required=False,
         widget=forms.TextInput(attrs={'placeholder': '01632 960481'}))
     email = forms.EmailField(
-        label='Email', validators=[validate_email],
+        label='Email*', validators=[validate_email],
         widget=forms.EmailInput(attrs={'placeholder': 'username@domain.co.uk'}))
     message = forms.CharField(
-        label='Message', max_length=500,
+        label='Message*', max_length=500,
         widget=forms.Textarea(attrs={'placeholder': 'Tell me how i can help you...'})
                             )
