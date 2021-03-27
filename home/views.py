7f9ce2f466dbdb3cba1c484c1cfed15b23ff17ad
Domchap90 <domchaple@gmail.com>
2021-03-27 11:28:53 +0000

    Sends message successfully and notifies user.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
+from django.contrib import messages
 from .forms import ContactForm
 
 
@@ -25,6 +27,24 @@ def portfolio(request):
 
 def contact(request):
     form = ContactForm()
+
+    if request.POST:
+        print(f"")
+        subject = "Message from contact page (DC webdeveloper)"
+        message = "Message sent:\n\n" + request.POST["message"] + "\n\n\n\
+Sent from: " + request.POST['name'] + "\nNumber given: \
+" + request.POST.get("number", '')
+        try:
+            send_mail(subject, message,
+                      request.POST.get("email"),
+                      ['info@dc-webdeveloper.com'],
+                      fail_silently=False)
+            messages.success(request, "Thank you for getting in touch. Your\
+ message has been sent.")
+        except IOError:
+            # Will catch both SMTPException AND socket.error
+            messages.error(request, "Unable to send email at this time. Please try again\
+later.")
 
     context = {
         'form': form,
